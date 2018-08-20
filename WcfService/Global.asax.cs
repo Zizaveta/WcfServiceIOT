dd7da9efe4643422747169179a886bff380f1f1d
Liza <miss.elizaveta.rengan@gmail.com>
2018-08-20 11:54:32 +0300

    Added Global

--- WcfService/Global.asax.cs
+++ WcfService/Global.asax.cs
@@ -22,7 +22,14 @@ namespace WcfService
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            // ljgbcfnb
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
